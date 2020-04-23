1a4b232b616da90efd69e8d3ab02446f046c56f8
“Yenn <yennlou21@gmail.com>
2020-04-23 15:00:37 +1200

    clean code

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs'
 import * as path from 'path'
 import imgcat = require('imgcat')
 import { Note } from '../src/interfaces'
@@ -6,46 +7,44 @@ import { unzip, readAnki2, deserializeCards } from '../src/readAnki'
 import { makeNoteHtml, makePng } from '../src/makePost'
 import { fetchAnkiDeck } from '../src/fetchAnkiDeck'
 
-const DECK_FOLDER = path.join(__dirname, 'decks')
-const DATA_FOLDER = path.join(__dirname, 'data')
-const POSTS_FOLDER = path.join(__dirname, 'posts')
-const DECK_PATH = path.join(DECK_FOLDER, 'mock-data.apkg')
-const ANKI2_PATH = path.join(DATA_FOLDER, 'collection.anki2')
-const POST_PATH = path.join(POSTS_FOLDER, 'anki.png')
+const DECK_KEY = 'mock-data.apkg'
+const DECK_PATH = path.join(__dirname, 'decks/mock-data.apkg')
+const TMP_FOLDER = path.join(__dirname, 'tmp')
+
 const NOTES_DATA: Note[] = [
   { content: 'question 1\u001fanswer 1' },
   { content: 'question 2\u001fanwser 2' }
 ]
 
 const trace = (message: string) => (data: any) => {
-  console.log(message, `input: ${JSON.stringify(data)}`)
+  console.log(message, `output: ${JSON.stringify(data)}`)
   return data
 }
 
 test('should return the notes from apkg file', async () => {
   await Promise.resolve()
-    .then(trace('start unzipping...'))
-    .then(unzip(DECK_PATH, DATA_FOLDER))
-    .then(trace('start reading anki2...'))
-    .then(readAnki2(ANKI2_PATH))
-    .then(trace('start deserialize the raw data...'))
+    .then(unzip(DECK_PATH, TMP_FOLDER))
+    .then(readAnki2(path.join(TMP_FOLDER, 'collection.anki2')))
     .then(deserializeCards)
-    .then(trace('test the result...'))
+    .then(trace('note extracted'))
     .then((notes) => {
       expect(notes).toEqual(NOTES_DATA)
     })
-    .finally(clearDir(DATA_FOLDER))
+    .finally(clearDir(TMP_FOLDER))
 })
 
 test('shoud make a png file from a note', async () => {
   await Promise.resolve(NOTES_DATA[0])
     .then(makeNoteHtml)
-    .then((html) => makePng(html, POST_PATH))
-    .then(() => imgcat(POST_PATH))
+    .then((html) => makePng(html, path.join(TMP_FOLDER, 'anki.png')))
+    .then(() => imgcat(path.join(TMP_FOLDER, 'anki.png')))
     .then(console.log)
-    .finally(clearDir(POSTS_FOLDER))
+    .finally(clearDir(TMP_FOLDER))
 })
 
 test.only('should fetch the anki deck', async () => {
-  await fetchAnkiDeck('mock-data.apkg', DECK_PATH)
+  await fetchAnkiDeck(DECK_KEY, path.join(TMP_FOLDER, DECK_KEY))
+    .then(() => fs.existsSync(path.join(TMP_FOLDER, DECK_KEY)))
+    .then(trace('deck fetched'))
+    .finally(clearDir(TMP_FOLDER))
 })
