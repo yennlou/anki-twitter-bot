8b7e1b996be3cacc8253abf67243eec7d7e02d49
“Yenn <yennlou21@gmail.com>
2020-04-23 16:23:10 +1200

    add test:should fetch the deck and make the post

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -42,9 +42,25 @@ test('shoud make a png file from a note', async () => {
     .finally(clearDir(TMP_FOLDER))
 })
 
-test.only('should fetch the anki deck', async () => {
+test('should fetch the anki deck', async () => {
   await fetchAnkiDeck(DECK_KEY, path.join(TMP_FOLDER, DECK_KEY))
     .then(() => fs.existsSync(path.join(TMP_FOLDER, DECK_KEY)))
     .then(trace('deck fetched'))
+    .finally(clearDir(TMP_FOLDER))
+})
+
+test.only('should fetch the deck and make the post', async () => {
+  const TMP_DECK_PATH = path.join(TMP_FOLDER, DECK_KEY)
+  const TMP_ANKI2_PATH = path.join(TMP_FOLDER, 'collection.anki2')
+  const TMP_IMAGE_PATH = path.join(TMP_FOLDER, 'anki.png')
+  await fetchAnkiDeck(DECK_KEY, TMP_DECK_PATH)
+    .then(unzip(TMP_DECK_PATH, TMP_FOLDER))
+    .then(readAnki2(TMP_ANKI2_PATH))
+    .then(deserializeCards)
+    .then((notes) => notes[0])
+    .then(makeNoteHtml)
+    .then((html) => makePng(html, TMP_IMAGE_PATH))
+    .then(() => imgcat(TMP_IMAGE_PATH))
+    .then(console.log)
     .finally(clearDir(TMP_FOLDER))
 })
